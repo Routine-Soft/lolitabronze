659e5c13ba5714b54da2167254cdd4b6771f8eae
Stanley Andrade <stanleyandrade95@gmail.com>
2026-08-28 13:53:54 -0300

    iconPath

--- electron-app/index.js
+++ electron-app/index.js
@@ -66,7 +66,7 @@ function createWindow() {
     width: 1280,
     height: 800,
     title: 'Lolita Bronze',
-    icon: path.join(__dirname, 'build/icon.ico'), // Garanta que o caminho aponta corretamente para o arquivo .ico
+    icon: iconPath, // Garanta que o caminho aponta corretamente para o arquivo .ico
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
